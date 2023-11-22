56429ff50c2a7f656ce44f9826fdc3fae7ca5db6
michelehobson <michelehobson@hotmail.com>
2023-11-22 15:37:20 -0500

    Post Not Working Yet

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -13,26 +13,53 @@ router.get('/', async (req, res) => {
     res.render('categoryView', { categories });
 })
 
-router.get('/:id', async (req, res) => {
-    const { id } = req.params;
-    const category = await Category.findById(id);
-    res.render('categoryView', { category });
+router.get('/newCat', async (req, res) => {
+    res.render('newCat')
 })
 
-router.get('/:id/patch', async (req, res) => {
-    const { id } = req.params;
-    const category = await Category.findById(id);
-    res.render('categoryView', { category, id });
+router.post('/categories', (req, res) => {
+    console.log(req.body)
+    res.send('making your cat')
 })
-router.get('/:id/post', async (req, res) => {
-    const { id } = req.params;
-    const category = await Category.findById(id);
-    res.render('categoryView', { category, id  });
-})
-router.get('/:id/delete', async (req, res) => {
-    const { id } = req.params;
-    const category = await Category.findById(id);
-    res.render('categoryView', { category, id  });
-})
+// router.get('/newCat', async (req, res) => {
+//     const categories = await Category.find({})
+//     console.log('HERE')
+//     res.render('newCat', { categories })
+// })
+
+// router.post('/categories/newCat', async (req, res) => {
+    // const catName = {
+    //     query: { _id: new ObjectId(req.params.id) },
+    //     name: req.body.categoryName
+    // };
+
+    // res.send({
+    //     userName: "bob",
+    //     desc: "recieived a POST request for user!",
+    //   });
+    
+    // res.render('newCat', { newCatVal });
+    // const newCatVal = new Category(req.body);
+    // await newCatVal.save();
+    // console.log(newCatVal); 
+//        res.render('newCat'), {};
+// router.get('/:id', async (req, res) => {
+//     const { id } = req.params;
+//     const category = await Category.findById(id);
+//     res.render('categoryView', { category });
+// })
+
+// router.get('/:id/patch', async (req, res) => {
+//     const { id } = req.params;
+//     const category = await Category.findById(id);
+//     res.render('categoryView', { category, id });
+// })
+// })
+
+// router.get('/:id/delete', async (req, res) => {
+//     const { id } = req.params;
+//     const category = await Category.findById(id);
+//     res.render('categoryView', { category, id  });
+// })
 
 module.exports = router;
